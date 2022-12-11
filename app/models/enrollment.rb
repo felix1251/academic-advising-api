db0a4ca73bb03b37279ff456f3b1533040fe620d
felix1251 <domhack123@gmail.com>
2022-12-11 10:45:56 +0800

    emrolled_id uniquness scoped

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,7 +3,7 @@ class Enrollment < ApplicationRecord
     belongs_to :enrolled, class_name: "Subject"
     belongs_to :academic_year
 
-    validates :enrolled_id, presence: true, uniqueness: true
+    validates :enrolled_id, presence: true,  uniqueness: { scope: :student_id }
     validates :check_grade, :if => :grade_changed?
 
     def check_grade
